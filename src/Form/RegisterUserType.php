d5e6dc9d426d2c31605e25595d0c9f5257604c0a
Harry EMARH <emarh.harry.code@gmail.com>
2024-12-20 22:40:36 +0100

    All Types added

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +16,31 @@ class RegisterUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email',
+                'attr' => [
+                    'placeholder' => 'Veuillez renseigner votre email'
+                ]
+            ])
             // ->add('roles')
-            ->add('password')
-            ->add('firstName')
-            ->add('lastName')
+            ->add('password', PasswordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Veuillez renseigner votre mot de passe'
+                ]
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Veuillez renseigner votre prénom'
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Veuillez renseigner votre nom'
+                ]
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
