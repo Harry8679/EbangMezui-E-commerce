00e055634563783e1a54257e25f639ad5dc045ae
Harry EMARH <emarh.harry.code@gmail.com>
2024-12-23 11:21:56 +0100

    index to login view

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends AbstractController
     #[Route('/connexion', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login/index.html.twig', [
+        return $this->render('login/login.html.twig', [
             'controller_name' => 'LoginController',
         ]);
     }
