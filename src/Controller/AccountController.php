def44f1aae8a882de677cda648bba7e06759a3e9
Harry EMARH <emarh.harry.code@gmail.com>
2024-12-24 15:21:13 +0100

    update password controller

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,6 +18,10 @@ class AccountController extends AbstractController
     #[Route('/compte/modifier-votre-mot-de-passe', name: 'app_account_modify_password')]
     public function password(): Response
     {
-        return $this->render('account/password.html.twig');
+        $form = $this->createForm(PasswordUserType::class);
+        
+        return $this->render('account/password.html.twig', [
+            'updatePasswordForm' => $form->createView()
+        ]);
     }
 }
