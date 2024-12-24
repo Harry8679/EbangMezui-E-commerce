6fdcd49e03012516a31c0dc0218291a188ec106b
Harry EMARH <emarh.harry.code@gmail.com>
2024-12-24 15:10:57 +0100

    Password User Type updated

--- src/Form/PasswordUserType.php
+++ src/Form/PasswordUserType.php
@@ -12,11 +12,7 @@ class PasswordUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
             ->add('password')
-            ->add('firstName')
-            ->add('lastName')
         ;
     }
 
