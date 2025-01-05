276f83a799b9d11c552815278742dceb080ca5c8
Harry EMARH <emarh.harry.code@gmail.com>
2025-01-05 01:41:50 +0100

    Category Crud Controller

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,16 +3,22 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CategoryCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
     {
         return Category::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Categorie')
+            ->setEntityLabelInPlural('Categories')
+        ;   
     }
 
     /*
