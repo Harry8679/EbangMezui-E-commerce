d18a6a5eca7c1176ec44b4bd03aa151d8cead693
Harry EMARH <emarh.harry.code@gmail.com>
2025-01-05 18:35:16 +0100

    Crud Product

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,7 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +19,20 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name')->setLabel('Nom')->setHelp('Nom de votre produit'),
+            SlugField::new('slug')->setLabel('URL')->setTargetFieldName('name')->setHelp('URL de votre produit est générée automatiquement'),
+            TextEditorField::new('description')->setLabel('Description')->setHelp('Description de votre produit'),
+            ImageField::new('illustration')->setLabel('Image')->setHelp('Image du produit en 600x600px.')->setBasePath('/uploads')->setUploadDir('public/uploads'),
+            NumberField::new('price')->setLabel('Prix H.T')->setHelp('Prix H.T du produit sans le sigle €'),
+            ChoiceField::new('tva')->setLabel('Taux de TVA')->setChoices([
+                '5.5%' => '5.5',
+                '10%' => '10',
+                '20%' => '20',
+            ]),
+            AssociationField::new('category', 'Catégorie associée')
         ];
     }
-    */
 }
