3fbcb2584fbc34df87c6b7c61275b5642c75a081
Harry EMARH <emarh.harry.code@gmail.com>
2025-01-08 00:04:09 +0100

    Cart class

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -1,3 +1,15 @@
 <?php
 
 namespace App\Classe;
+
+class Cart
+{
+    public function add($id)
+    {
+        // Appeler la session de symfony
+
+        // Créer ma session Cart
+
+        // Ajouter une quantité +1 à mon produit
+    }
+}
