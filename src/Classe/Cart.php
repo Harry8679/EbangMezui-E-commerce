bd872606ef91cf05fda2ddb31adc1315659c3bb2
Harry EMARH <emarh.harry.code@gmail.com>
2025-01-08 00:46:33 +0100

    get session

--- src/Classe/Cart.php
+++ src/Classe/Cart.php
@@ -2,15 +2,27 @@
 
 namespace App\Classe;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+
 class Cart
 {
+    public function __construct(private RequestStack $requestStack)
+    {
+        
+    }
+    
     public function add($product)
     {
-        dd($product);
         // Appeler la session de symfony
-
-        // Créer ma session Cart
+        $session = $this->requestStack->getSession();
 
         // Ajouter une quantité +1 à mon produit
+        $cart[$product->getId()] = [
+            'object' => $product,
+            'quantity' => 1
+        ];
+        
+        // Créer ma session Cart
+        $this->requestStack->getSession()->set('cart', $cart);
     }
 }
