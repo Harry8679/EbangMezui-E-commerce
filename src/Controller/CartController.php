b78d6210927625e76c78ff5ccf6174b418efc11b
Harry EMARH <emarh.harry.code@gmail.com>
2025-01-08 19:44:30 +0100

    add updated

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Classe\Cart;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -19,7 +20,7 @@ class CartController extends AbstractController
     }
 
     #[Route('/cart/add/{id}', name: 'app_cart_add')]
-    public function add($id, Cart $cart, ProductRepository $productRepository): Response
+    public function add($id, Cart $cart, ProductRepository $productRepository, Request $request): Response
     {
         $product = $productRepository->findOneById($id);
 
@@ -31,9 +32,10 @@ class CartController extends AbstractController
 
         $this->addFlash('success', 'Produit correctement ajouté à votre panier.');
 
-        return $this->redirectToRoute('app_product', [
-            'slug' => $product->getSlug()
-        ]);
+        // return $this->redirectToRoute('app_product', [
+        //     'slug' => $product->getSlug()
+        // ]);
+        return $this->redirect($request->headers->get('referer'));
     }
 
     #[Route('/cart/remove', name: 'app_cart_remove')]
